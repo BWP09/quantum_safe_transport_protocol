e0d7b08fd170289acfe7240f9d18cc6923427fc1
BWP09 <bwpayne2@gmail.com>
2025-06-05 22:28:38 -0700

    update test

--- test.py
+++ test.py
@@ -1,16 +1,1 @@
-from typing import overload
-
-class Test:
-    @overload
-    def test(self, address: str):
-        ...
-
-    @overload
-    def test(self, one: int, two: int):
-        ...
-
-    def test(self, one: int | str | None = None, two: int | None = None, address: int | str | None = None):
-        print(one, two)
-
-Test().test("test")
-Test().test(1, 2)
+#TODO: remove
